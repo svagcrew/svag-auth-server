59bf45cce42bb4e53b2b8846b862bd6a7b4cb7b1
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-21 18:56:00 +0400

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import crypto from 'crypto'
 import type { Express } from 'express'
 import jwt from 'jsonwebtoken'
-import { Passport } from 'passport'
-import { ExtractJwt, Strategy as JWTStrategy } from 'passport-jwt'
 
 type LikeRequest = Record<string, any>
 
@@ -10,7 +8,6 @@ export const createAuthThings = <
   TAppContext,
   TJwtPayload extends object | string,
   TMeFromJwtPayload,
-  TMe = TMeFromJwtPayload,
   TExpress extends Express = Express,
   TRequest extends LikeRequest = LikeRequest,
 >({
@@ -18,14 +15,16 @@ export const createAuthThings = <
   passwordSalt,
   normalizeJwtPayload,
   getMeFromJwtPayload,
-  getMeByJwtPayloadAndRequest = (meFromJwtPayload) => meFromJwtPayload as any,
   tokenCookieName,
 }: {
   jwtSecret: string
   passwordSalt: string
   normalizeJwtPayload: (jwtPayload: any) => TJwtPayload
-  getMeFromJwtPayload: (jwtPayload: TJwtPayload, ctx: TAppContext) => Promise<TMeFromJwtPayload>
-  getMeByJwtPayloadAndRequest?: (meFromJwtPayload: TMeFromJwtPayload, req: TRequest) => Promise<TMe> | TMe
+  getMeFromJwtPayload: (
+    jwtPayload: TJwtPayload,
+    ctx: TAppContext,
+    req?: TRequest
+  ) => Promise<TMeFromJwtPayload> | TMeFromJwtPayload
   tokenCookieName: string
 }) => {
   const signJwt = (jwtPayload: any) => {
@@ -33,51 +32,30 @@ export const createAuthThings = <
     return jwt.sign(normalizedPayload, jwtSecret)
   }
 
+  const parseJwt = (token: string) => {
+    const rawJwtPayload = jwt.verify(token, jwtSecret)
+    return normalizeJwtPayload(rawJwtPayload)
+  }
+
   const getPasswordHash = (password: string) => {
     return crypto.createHash('sha256').update(`${passwordSalt}${password}`).digest('hex')
   }
 
   const applyAuthToExpressApp = ({ expressApp, ctx }: { expressApp: TExpress; ctx: TAppContext }): void => {
-    const passport = new Passport()
-
-    passport.use(
-      new JWTStrategy(
-        {
-          secretOrKey: jwtSecret,
-          jwtFromRequest: (req) => {
-            if (req.headers.authorization?.startsWith('Bearer ')) {
-              return ExtractJwt.fromAuthHeaderWithScheme('Bearer')(req)
-            } else if (req.cookies[tokenCookieName]) {
-              return req.cookies[tokenCookieName]
-            }
-            return null
-          },
-        },
-        (jwtPayload: any, done) => {
-          void (async () => {
-            try {
-              done(null, await getMeFromJwtPayload(normalizeJwtPayload(jwtPayload), ctx))
-            } catch (error) {
-              done(error, false)
-            }
-          })()
-        }
-      )
-    )
-
     expressApp.use((req: any, res: any, next: any) => {
-      passport.authenticate('jwt', { session: false }, (...args: any[]) => {
-        const maybePromise = getMeByJwtPayloadAndRequest(args[1], req)
-        const promise = maybePromise instanceof Promise ? maybePromise : Promise.resolve(maybePromise)
-        promise
-          .then((me) => {
-            ;(req as any).me = me
-            next()
-          })
-          .catch((error) => {
-            next(error)
-          })
-      })(req, res, next)
+      const token = req.headers?.authorization?.startsWith('Bearer ')
+        ? req.headers.authorization.slice(7)
+        : req.cookies[tokenCookieName]
+      const jwtPayload = token ? parseJwt(token) : normalizeJwtPayload(undefined)
+      void (async () => {
+        try {
+          const meFromJwtPayload = await getMeFromJwtPayload(jwtPayload, ctx, req)
+          ;(req as any).me = meFromJwtPayload
+          next()
+        } catch (error) {
+          next(error)
+        }
+      })()
     })
   }
 
@@ -86,6 +64,5 @@ export const createAuthThings = <
     signJwt: signJwt as (jwtPayload: TJwtPayload) => string,
     getPasswordHash,
     getMeFromJwtPayload,
-    getMeByJwtPayloadAndRequest,
   }
 }
